4494b6a2b8838a7405245039aacc6e5083c05579
Luiz Reis <reisluiz05@gmail.com>
2023-05-13 18:59:50 -0300

    Project created

---
+++ src/main/java/com/luizreis/injecaodedependencia/InjecaoDeDependenciaApplication.java
@@ -0,0 +1,13 @@
+package com.luizreis.injecaodedependencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjecaoDeDependenciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InjecaoDeDependenciaApplication.class, args);
+    }
+
+}
